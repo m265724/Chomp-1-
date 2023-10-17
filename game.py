17360579de25d004ca9b0cfa50b509c240d67085
m265724 <148244273+m265724@users.noreply.github.com>
2023-10-17 10:54:42 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,37 @@
 import pygame
 
-print(f"Programming is fun")
+print(f"Running make_screen.py")
+
+#Screen dimensions
+screen_width = 800
+screen_height = 600
+
+#Colors
+blue = (0,0,255)
+brown = (139,69,19)
+
+#Create the screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Blue Background with Brown Rectangle")
+
+#Main loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #Fill screen with blue
+    screen.fill(blue)
+
+    #Draw brown rectangle
+    rectangle_height = 100
+    pygame.draw.rect(screen, brown, (0 ,screen_height - rectangle_height, screen_width, rectangle_height))
+
+    #Update the display
+    pygame.display.flip()
+
+#Quit
+pygame.quit()
+
+print(f"End of code")
