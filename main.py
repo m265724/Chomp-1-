d42eb3be19eba9637ffe40b34590013b2c63eaa3
m265724 <148244273+m265724@users.noreply.github.com>
2023-10-24 11:26:54 -0400

    Moving fish

--- main.py
+++ main.py
@@ -26,8 +26,15 @@ background = scr.copy()
 make_background(background)
 
 # Create splash screen
-make_splash_screen(background, scr)
+# make_splash_screen(background, scr)
 
+# Create a single fish
+fish_img = pygame.image.load('assests/sprites/green_fish.png').convert()
+fish_img.set_colorkey((0,0,0)) # makes area around fish transparent
+fish_x = 0
+fish_x_dir = 1
+fish_y = 0
+fish_y_dir = 1
 
 print('Running Game ----')
 
@@ -42,6 +49,22 @@ while running:
 
     # Draw background.
     scr.blit(background, (0, 0))
+
+    #Update fish's positions
+    fish_x += 0.1*fish_x_dir
+    fish_y  = 0*fish_y_dir
+
+    # Move fish on screen
+    if fish_x >= scr.get_width() - fish_img.get_width():
+        fish_x_dir = -1
+        fish_img = pygame.transform.flip(fish_img, True, False)
+
+    if fish_x < 0:
+        fish_x_dir = 1
+        fish_img = pygame.transform.flip(fish_img, True, False)
+
+    # Draw fish
+    scr.blit(fish_img, (fish_x,fish_y))
 
     # Update the display
     pygame.display.flip()
